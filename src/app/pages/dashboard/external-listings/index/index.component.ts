2c221d21f3401338916b8ff1109fa4eca97e44c2
PaschalDedukt <paschal.okafor@dedukt.co>
2026-02-22 16:50:15 +0100

    add pre-push hooks

--- src/app/pages/dashboard/external-listings/index/index.component.ts
+++ src/app/pages/dashboard/external-listings/index/index.component.ts
@@ -9,36 +9,42 @@ import { User } from '@shared/services/types';
 import { AgGridAngular } from 'ag-grid-angular';
 import { ColDef, GridOptions } from 'ag-grid-community';
 import { Observable, Subject, takeUntil } from 'rxjs';
-import { CreateAndDownloadExternalListingsBtnsComponent } from "../../../../core/components/dashboard/create-and-download-external-listings-btns.component";
+import { CreateAndDownloadExternalListingsBtnsComponent } from '../../../../core/components/dashboard/create-and-download-external-listings-btns.component';
 
 @Component({
   selector: 'external-listing-index',
   standalone: true,
-  imports: [TextButtonComponent, AgGridAngular, CreateAndDownloadExternalListingsBtnsComponent],
+  imports: [
+    TextButtonComponent,
+    AgGridAngular,
+    CreateAndDownloadExternalListingsBtnsComponent,
+  ],
   templateUrl: './index.component.html',
-  styleUrl: './index.component.scss'
+  styleUrl: './index.component.scss',
 })
 export class IndexComponent implements OnDestroy {
   gridOptions: GridOptions = {
     suppressFieldDotNotation: true, // Treats dot as part of the field name
-    rowBuffer: 5,
-    getRowId: params => `${params.data.id}`,
+    rowBuffer: 10,
+    getRowId: (params) => `${params.data.id}`,
     overlayNoRowsTemplate: `<span class="ag-overlay-loading-center">No External listing Available</span>`,
-
   };
   rowData!: Observable<any | null>;
   colDefs: ColDef<any>[] = [
     {
-      headerName: 'S/N', width: 75, filter: false, field: 'id',
+      headerName: 'S/N',
+      width: 75,
+      filter: false,
+      field: 'id',
       cellRenderer(params: any) {
         if (params.value) {
-          return params.node.rowIndex + 1
+          return params.node.rowIndex + 1;
         }
-      }
+      },
     },
     {
       field: 'Date',
-      sortable: false
+      sortable: false,
     },
     {
       field: 'Region',
@@ -50,92 +56,92 @@ export class IndexComponent implements OnDestroy {
       field: 'Section',
     },
     {
-      field: "L.G.A",
-      headerName: "L. G. A",
-      filter: false
+      field: 'L.G.A',
+      headerName: 'L. G. A',
+      filter: false,
     },
     {
-      field: "L.C.D.A",
-      headerName: "L. C. D. A",
-      filter: false
+      field: 'L.C.D.A',
+      headerName: 'L. C. D. A',
+      filter: false,
     },
     {
-      field: "Street",
-      filter: false
+      field: 'Street',
+      filter: false,
     },
     {
-      field: "Street Number",
-      filter: false
+      field: 'Street Number',
+      filter: false,
     },
     {
-      field: "Development",
+      field: 'Development',
     },
     {
-      field: "Sector",
+      field: 'Sector',
     },
     {
-      field: "Type",
+      field: 'Type',
     },
     {
-      field: "Sub Type",
+      field: 'Sub Type',
     },
     {
-      field: "No of Beds",
+      field: 'No of Beds',
     },
     {
-      field: "Size",
-      cellClass: "text-center",
-      headerClass: "text-center"
+      field: 'Size',
+      cellClass: 'text-center',
+      headerClass: 'text-center',
     },
     {
-      field: "Land Area",
-      cellClass: "text-center",
-      headerClass: "text-center",
+      field: 'Land Area',
+      cellClass: 'text-center',
+      headerClass: 'text-center',
     },
     {
-      field: "Offer",
+      field: 'Offer',
     },
     {
-      field: "Sale Price",
-      cellClass: 'text-right'
+      field: 'Sale Price',
+      cellClass: 'text-right',
     },
     {
-      field: "Lease Price",
-      cellClass: 'text-right'
+      field: 'Lease Price',
+      cellClass: 'text-right',
     },
     {
-      field: "Price/Sqm",
-      cellClass: 'text-right'
+      field: 'Price/Sqm',
+      cellClass: 'text-right',
     },
     {
-      field: "Service Charge",
-      cellClass: 'text-right'
+      field: 'Service Charge',
+      cellClass: 'text-right',
     },
     {
-      field: "Developer",
+      field: 'Developer',
     },
     {
-      field: "Listing Agent",
+      field: 'Listing Agent',
       width: 250,
     },
     {
-      field: "Contact Number",
+      field: 'Contact Number',
     },
     {
-      field: "E-mail",
-      width: 250
-    },
-    {
-      field: "Comment",
+      field: 'E-mail',
       width: 250,
-      filter: false
     },
     {
-      field: "Source",
-      width: 200
+      field: 'Comment',
+      width: 250,
+      filter: false,
     },
     {
-      field: "Updated By",
+      field: 'Source',
+      width: 200,
+    },
+    {
+      field: 'Updated By',
       hide: this.permission.isAdhocStaff,
     },
   ];
@@ -143,24 +149,28 @@ export class IndexComponent implements OnDestroy {
     sortable: true,
     filter: true,
     filterParams: {
-      debounceMs: 500,     // Debounce time for user input
-      filterOptions: ['contains']
+      debounceMs: 500, // Debounce time for user input
+      filterOptions: ['contains'],
     },
     autoHeight: false,
     cellClass: '!flex !items-center',
-    cellStyle: { 'white-space': 'normal', 'word-wrap': 'break-word', 'height': 'max-content' },
+    cellStyle: {
+      'white-space': 'normal',
+      'word-wrap': 'break-word',
+      height: 'max-content',
+    },
     width: 150,
   };
   tableThemeColor: 'dark' | 'light' = 'light';
   isLoading = true;
-  pageSize = 500
-  initialRowCount = 0
+  pageSize = 500;
+  initialRowCount = 0;
   datasource: any;
-  totalRecords = '...'
-  destroy$ = new Subject<void>()
-  currentUser!: User
+  totalRecords = '...';
+  destroy$ = new Subject<void>();
+  currentUser!: User;
 
-  dataCache: Map<string, { data: any, totalRecords: number }> = new Map()
+  dataCache: Map<string, { data: any; totalRecords: number }> = new Map();
   gridApi!: any;
 
   onGridReady(params: any) {
@@ -174,86 +184,93 @@ export class IndexComponent implements OnDestroy {
     public colorScheme: ColorSchemeService,
     public permission: PermissionService,
     private authService: AuthService,
-    private cdr: ChangeDetectorRef
-  ) { }
+    private cdr: ChangeDetectorRef,
+  ) {}
 
   ngOnInit() {
-
-    this.authService.onCurrentUser().pipe(takeUntil(this.destroy$)).subscribe((v) => {
-      this.currentUser = v!
-    })
+    this.authService
+      .onCurrentUser()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((v) => {
+        this.currentUser = v!;
+      });
 
     const paginatedListingParams: PaginatedListingsParams = {
-      limit: 1
-    }
+      limit: 1,
+    };
 
     if (!this.permission.isAdmin) {
-      paginatedListingParams.updatedById = this.currentUser.id
+      paginatedListingParams.updatedById = this.currentUser.id;
     }
 
-    this.els.apiGetPaginatedListings(paginatedListingParams).pipe(takeUntil(this.destroy$))
-      .subscribe(v => {
-        this.totalRecords = v.totalPages.toString()
-      })
+    this.els
+      .apiGetPaginatedListings(paginatedListingParams)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((v) => {
+        this.totalRecords = v.totalPages.toString();
+      });
 
     this.datasource = {
       getRows: (params: any) => {
-        this.isLoading = true
-        this.cdr.detectChanges()
+        this.isLoading = true;
+        this.cdr.detectChanges();
         const startRow = params.startRow;
-        const currentPage = (Math.floor(startRow / this.pageSize) + 1); // Ensure page calculation works for both up and down scrolls
+        const currentPage = Math.floor(startRow / this.pageSize) + 1; // Ensure page calculation works for both up and down scrolls
         const paginatedListingParams: PaginatedListingsParams = {
           limit: this.pageSize,
           currentPage,
-        }
+        };
         if (Object.keys(params.filterModel).length > 0) {
-          paginatedListingParams.agFilterModel = params.filterModel
+          paginatedListingParams.agFilterModel = params.filterModel;
           this.setFilterModelToLocalStorage(params.filterModel);
         }
         if (params.sortModel.length > 0) {
-          const sort = params.sortModel[0].sort
-          const colId = params.sortModel[0].colId
-          const sortBy = `${colId}:${sort}`
-          paginatedListingParams.sortBy = sortBy
+          const sort = params.sortModel[0].sort;
+          const colId = params.sortModel[0].colId;
+          const sortBy = `${colId}:${sort}`;
+          paginatedListingParams.sortBy = sortBy;
         }
 
         if (!this.permission.isAdmin) {
-          paginatedListingParams.updatedById = this.currentUser.id
+          paginatedListingParams.updatedById = this.currentUser.id;
         }
 
-        const cachedData = this.getCachedData(paginatedListingParams)
+        const cachedData = this.getCachedData(paginatedListingParams);
         if (cachedData) {
           params.successCallback(cachedData.data, cachedData.totalRecords);
 
-          this.isLoading = false
-          this.cdr.detectChanges()
-        }
-
-        else {
-
-          this.els.apiGetPaginatedListings(paginatedListingParams).pipe(takeUntil(this.destroy$))
+          this.isLoading = false;
+          this.cdr.detectChanges();
+        } else {
+          this.els
+            .apiGetPaginatedListings(paginatedListingParams)
+            .pipe(takeUntil(this.destroy$))
             .subscribe({
               next: (value) => {
                 params.successCallback(value.data, value.totalRecords);
                 if (value.data.length === 0) {
                   Promise.resolve().then(() => {
                     this.gridApi.showNoRowsOverlay();
-                  })
+                  });
                 }
 
-                this.cacheData(paginatedListingParams, value.data, value.totalRecords)
+                this.cacheData(
+                  paginatedListingParams,
+                  value.data,
+                  value.totalRecords,
+                );
                 this.isLoading = false;
-                this.cdr.detectChanges()
+                this.cdr.detectChanges();
               },
               error: () => {
                 params.failCallback();
                 this.isLoading = false;
-                this.cdr.detectChanges()
-              }
-            })
+                this.cdr.detectChanges();
+              },
+            });
         }
-      }
-    }
+      },
+    };
 
     this.colorScheme
       .getActualColorScheme()
@@ -268,21 +285,28 @@ export class IndexComponent implements OnDestroy {
   }
 
   onRowClicked(ev: any) {
-    const data = ev.data
-    this.router.navigateByUrl(`/dashboard/external-listings/${data.id}`, data)
+    const data = ev.data;
+    this.router.navigateByUrl(`/dashboard/external-listings/${data.id}`, data);
   }
 
-  cacheData(paginatedListingParams: PaginatedListingsParams, data: any, totalRecords: number) {
-    this.dataCache.set(JSON.stringify(paginatedListingParams), { data, totalRecords })
+  cacheData(
+    paginatedListingParams: PaginatedListingsParams,
+    data: any,
+    totalRecords: number,
+  ) {
+    this.dataCache.set(JSON.stringify(paginatedListingParams), {
+      data,
+      totalRecords,
+    });
   }
 
   getCachedData(paginatedListingParams: PaginatedListingsParams) {
-    return this.dataCache.get(JSON.stringify(paginatedListingParams))
+    return this.dataCache.get(JSON.stringify(paginatedListingParams));
   }
 
   ngOnDestroy(): void {
-    this.destroy$.next()
-    this.destroy$.complete()
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
   private getFilterModelFromLocalStorage(): any {
@@ -291,7 +315,9 @@ export class IndexComponent implements OnDestroy {
   }
 
   private setFilterModelToLocalStorage(filterModel: any) {
-    sessionStorage.setItem('externalListingsFilterModel', JSON.stringify(filterModel));
+    sessionStorage.setItem(
+      'externalListingsFilterModel',
+      JSON.stringify(filterModel),
+    );
   }
-
 }
